8a237b4e14ebd2d6ae61ad98f0543158cd77dd10
Jen Luo <jaepoobear@gmail.com>
2023-08-29 15:28:51 -0700

    adding config files

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    theme: {
+      spacing: {
+        '13': '3.25rem',
+        '15': '3.75rem',
+        '128': '32rem',
+        '144': '36rem',
+      }
+    },
+    extend: {
+      colors: {
+        'card-yellow': '#FBFF38',
+        'card-green': '#867CFA',
+        'card-purple': '#A9D96C'
+      }
+    },
+  },
+  plugins: [],
+}
